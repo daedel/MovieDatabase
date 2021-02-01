4dd77b0a2d59911573c5e40d70a0b62b9ba76270
Damian Gasiorowski <dgasiorowski@perceptyx.com>
2021-02-01 11:39:21 +0100

    Added main auth views(login, logout, sign up)

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.template.response import TemplateResponse
+from django.views.generic import TemplateView
+
+from MainApp.services import get_movie_info
+
+
+class MainView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = {
+            'movie_info': get_movie_info(),
+        }
+        return context
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registration.html', {'form': form})
